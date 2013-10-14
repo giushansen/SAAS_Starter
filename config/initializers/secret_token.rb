fc386990a72078b17c3b05d749385b74226caf4f
Guillaume Fourret <guillaume@Guillaumes-MacBook-Pro.local>
2013-10-15 10:16:28 +1100

    First commit with rails -new starter-app -d postgresql -T. No tests. Git Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StarterApp::Application.config.secret_key_base = '2267ae3f70953cb3ea05d0d32c874c3a3eeba1234b8fde7c995dfd33e2c00267db83fba321fcc05654b2e87801ab07b1a49f8176bee4e4e1b1a05d0d699d9d54'
